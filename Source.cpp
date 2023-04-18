64d50b4a1bb7dbe789e1eeb45744bbb705682e57
funnynickname2010 <128163982+funnynickname2010@users.noreply.github.com>
2023-04-18 21:40:40 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,47 +1,153 @@
-#include <iostream>
 #include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+#define NAME_LENGTH 30
+#define SETW_PARAM 6
+#define STOCK_LEN 30
 
 using namespace std;
 
-struct product {
-	char name[20];
-	double cost = 0;
-	int quantity = 0;
+struct Product
+{
+public:
+	char name[NAME_LENGTH];
+	int ln = NAME_LENGTH;
+	double price = -1;
+	int quantity = -1;
 };
 
-void product_getter(product position);
+Product Product_input();
+void Product_print(const Product& myProduct);
+int user_interface();
 
 int main()
 {
-	int n = 200; //Arbitrary number for the initialization of the stock array
+	int first_boot_flag = 1;
 
-	product* stock;
-	stock = new product[n];
+	if (first_boot_flag == 1)
+	{
+		struct Product* stock = new Product[STOCK_LEN];
+		int first_empty_position_stock = 0;
 
-	product a;
-	product_getter(a);
+		int user_intention = user_interface();
 
-	cout << a.name << endl << a.cost << endl << a.quantity << endl;
+		if (user_intention == 1) //Append a product to the stock
+		{
+			stock[first_empty_position_stock] = Product_input();
+			first_empty_position_stock++;
+		}
+		else if (user_intention == 2) //Product's data
+		{
+			//Search and compare                                 //Show of way?
+			//Product_print();
+		}
+		else if (user_intention == 3) //Add a product to the bucket
+		{
+
+		}
+		else if (user_intention == 4) //Delete a product from the bucket
+		{
+
+		}
+		else if (user_intention == 5) //Delete a product from the stock
+		{
+
+		}
+		else if (user_intention == 6) //Checkout
+		{
+
+		}
+		else if (user_intention == 7) //Exit
+		{
+
+		}
+	}
 }
 
-void product_getter(product position)
+int str_compare(const char*& op1, const char*& op2)
+{//                                                     Should implement try catch
+	int res = -2;
+
+	for (int i = 0;; i++)
+	{
+		if (op1[i] == '\0' and op2[i] != '\0')
+		{
+			res = 0;
+			break;
+		}
+		else if (op1[i] != '\0' and op2[i] == '\0')
+		{
+			res = 1;
+			break;
+		}
+		else if (op1[i] == '\0' and op2[i] == '\0')
+		{
+			res = -1;
+			break;
+		}
+	}
+
+	return res;
+}
+
+int user_interface()
 {
-	//Get the name, tried dynamic memory but it looks really really ugly
+	cout << "--------------INTERFACE---------------" << endl;
+	cout << " Options: " << endl;
+	cout << "1. Append a product to the stock" << endl;
+	cout << "2. Product's data" << endl;
+	cout << "3. Add a product to the bucket" << endl;
+	cout << "4. Delete a product from the bucket" << endl;
+	cout << "5. Delete a product from the stock" << endl;
+	cout << "6. Checkout" << endl;
+	cout << "7. Exit" << endl;
+
+	int res;
+	cin >> res;
+
+	return res;
+}
+
+Product Product_input()
+{
+	Product myProduct;
+
+	//Inputting name
+
+	char* service_name_array = new char[myProduct.ln];
 
 	cout << "Input name: ";
-	cin >> position.name;
-	cout << endl;
+	cin.get(service_name_array, myProduct.ln);
 
-	//Get cost
+	int i = 0;
+	while (service_name_array[i - 1] != '\0') //Really it's valid data
+	{
+		myProduct.name[i] = service_name_array[i];
+		i++;
+	}
 
-	cout << "Input cost: ";
-	cin >> position.cost;
-	cout << endl;
+	delete[] service_name_array;
 
-	//Get quantity
+	//Inputting price and quantity
 
+	cout << "Input price: ";
+	cin >> myProduct.price;
 	cout << "Input quantity: ";
-	cin >> position.quantity;
+	cin >> myProduct.quantity;
 	cout << endl;
+
+	return myProduct;
 }
 
+void Product_print(const Product& myProduct)
+{
+	int i = 0;
+	while (myProduct.name[i] != '\0')
+	{
+		cout << myProduct.name[i];
+		i++;
+	}
+
+	cout << setw(SETW_PARAM) << myProduct.price << setw(SETW_PARAM) << myProduct.quantity << endl;
+}
